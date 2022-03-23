0b0b8061aa1df98202681c046ad07634904125b9
Dani Almaraz <dtalmaraz@gmail.com>
2022-03-22 20:18:43 -0700

    added pseudocode for testing and fixed user sign up
    Co-authored-by: Erica <ericaysoh@gmail.com>
    Co-authored-by: Zack <zackdaniels2522@gmail.com>
    Co-authored-by: Ming <yuyanming1994@hotmail.com>
    Co-authored-by: Dani <dtalmaraz@gmail.com>
    Co-authored-by: Ryan <ryanwallace1396@gmail.com>

---
+++ __tests__/react.js
@@ -0,0 +1,35 @@
+//Unit testing react components
+
+//Inventory Component
+//TODO: Test the following:
+//1. The page loads with five h4 elements, with the values from props.invInfo of following:
+//id, name, quanitity, category, location
+//2. The page loads with two buttons, 'Update' and 'Delete'
+//3. The updatedItem function should be invoked when 'Update' button is clicked (is it being invoked here or just passed down?)
+//4. The deletedItem function should be invoked when 'Delete' button is clicked
+
+//Inventory Display Component
+//TODO: Test the following:
+//1. The page loads with five h4 elements, with following text:
+//'ID', 'NAME', 'QUANTITY', 'CATEGORY', 'LOCATION'
+//2. The addedItem function should be invoked when 'Add Item' button is clicked
+
+//Login Component
+//TODO: Test the following:
+//1. The page loads with h2 element with text 'Please sign in'
+//2. The page renders input containers
+//3. The page renders submit button
+//4. The handleSubmit function is invoked when submit button is clicked
+//5. Need to test that the user is authenticated (put this should probably take place in supertests) 
+
+//Login Signup
+//TODO: Test the following:
+//1.  If isShow variable is true, render a link with text 'Not a member? Sign-Up here!'
+//2.  If isShow variable is false, render a link with text 'Already a member? Log-In here!'
+
+//NavBarLoggedOut
+
+//NavBarLoggedIn
+
+
+//Sign up

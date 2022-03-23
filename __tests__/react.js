808a302275f36e23726e5004a4ac137df5de66ac
Dani Almaraz <dtalmaraz@gmail.com>
2022-03-23 12:36:26 -0700

    Test files
    Co-authored by: Zack <zackdaniels2522@gmail.com>
    Co-authored by: Erica <ericaysoh@gmail.com>
    Co-authored by: Dani <dtalmaraz@gmail.com>

--- __tests__/react.js
+++ __tests__/react.js
@@ -1,18 +1,22 @@
 //Unit testing react components
+// import React from 'React';
+// import userEvent from '@testing-library/user-event';
+// import { render, screen, waitFor } from '@testing-library/react';
+// import regeneratorRuntime from 'regenerator-runtime';
 
 //Inventory Component
 //TODO: Test the following:
 //1. The page loads with five h4 elements, with the values from props.invInfo of following:
 //id, name, quanitity, category, location
 //2. The page loads with two buttons, 'Update' and 'Delete'
-//3. The updatedItem function should be invoked when 'Update' button is clicked (is it being invoked here or just passed down?)
-//4. The deletedItem function should be invoked when 'Delete' button is clicked
+//3. The updatedItem function should update the item when 'Update' button is clicked 
+//4. The deletedItem function should delete the item when 'Delete' button is clicked
 
 //Inventory Display Component
 //TODO: Test the following:
 //1. The page loads with five h4 elements, with following text:
 //'ID', 'NAME', 'QUANTITY', 'CATEGORY', 'LOCATION'
-//2. The addedItem function should be invoked when 'Add Item' button is clicked
+//2. The addedItem function should add the item when 'Add Item' button is clicked
 
 //Login Component
 //TODO: Test the following:
@@ -20,7 +24,7 @@
 //2. The page renders input containers
 //3. The page renders submit button
 //4. The handleSubmit function is invoked when submit button is clicked
-//5. Need to test that the user is authenticated (put this should probably take place in supertests) 
+//5. Need to test that the user is authenticated (but this should probably take place in supertests) 
 
 //Login Signup
 //TODO: Test the following:

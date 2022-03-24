28d55e58b090028c2eba73f7dd01dc3142c0b05e
Erica Oh <ericaysoh@gmail.com>
2022-03-23 20:14:05 -0700

    exploring testing
    Co-authored-by: Dani Talmaraz <dtalmaraz@gmail.com>
    Co-authored-by: Zack Daniels <zackdaniels2522@gmail.com>
    Co-authored-by: Erica Oh <ericysoh@gmail.com>
    Co-authored-by: Ryan Wallace<ryanwallace1396@gmail.com>
    Co-authored by: <yuyanming1994@hotmail.com>

--- server/routes/accounts.js
+++ server/routes/accounts.js
@@ -5,14 +5,14 @@ const accountsController = require('../controllers/accountsController');
 
 
 // handle get requests to the root path with a param - get a single account (NOT CURRENTLY USED IN FRONT END)
-router.get('/:name', accountsController.getAnAccount, (req,res) => {
-  res.status(200).json({account: res.locals.account})
-});
+// router.get('/:name', accountsController.getAnAccount, (req,res) => {
+//   res.status(200).json({account: res.locals.account})
+// });
 
-// handle get requests to the root path - gets all accounts information (USED WHEN SIGNING UP)
-router.get('/', accountsController.getAllAccounts, (req,res) => {
-  res.contentType('application/json').status(200).json({accounts: res.locals.accounts});
-});
+// // handle get requests to the root path - gets all accounts information (USED WHEN SIGNING UP)
+// router.get('/', accountsController.getAllAccounts, (req,res) => {
+//   res.contentType('application/json').status(200).json({accounts: res.locals.accounts});
+// });
 
 // handle post requests to the signup path - Creates an Account (USED WHEN SIGNING UP)
 router.post('/signup', accountsController.createAccount, (req,res) => {
